c5e1e61e8cdbf5374b6f3b41f8430f294e7bfbe3
Stefan Danzl <stefan.danzl@live.de>
2023-10-29 12:43:06 +0100

    Template Svelte + Tabulator

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  //@ts-nocheck
+import Table from "./Table.svelte"
+import {DateTime} from 'luxon'
+
+var columns = [ //Define Table Columns
+	 	{title:"Name", field:"name", width:150},
+	 	{title:"Age", field:"age", hozAlign:"left", formatter:"progress"},
+	 	{title:"Favourite Color", field:"col"},
+	 	{title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
+ 	]
+
+   var data = [
+ 	{id:1, name:"Oli Bob", age:"12", col:"red", dob:"1960/12/23"},
+ 	{id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
+ 	{id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
+ 	{id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
+ 	{id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
+ ];
+
+
+
+</script>
+<main>
+
+{#if data && columns}
+  <Table {data} {columns}/>
+{/if}
+</main>
+<style>
+
+</style>
---
+++ src/Table.svelte
@@ -0,0 +1,30 @@
+<script>
+    //@ts-nocheck
+    import {TabulatorFull as Tabulator} from 'tabulator-tables';
+    import {onMount} from 'svelte';
+    import {DateTime} from 'luxon'
+    
+  
+    export let data, columns;
+  
+    let tableComponent;
+  
+    onMount(() => {
+      var table = new Tabulator(tableComponent, {
+        data: data, //link data to table
+        reactiveData:true, //enable data reactivity
+        columns: columns, //define table columns
+        
+      });
+      table.on('rowClick', (e, row) => {
+  	alert("Row " + row.getIndex() + " Clicked!!!!");
+  })
+    });
+  </script>
+  
+  
+  <div bind:this={tableComponent}></div>
+  
+  <svelte:head>
+    <link href="https://unpkg.com/tabulator-tables@4.9.1/dist/css/tabulator.min.css" rel="stylesheet">
+  </svelte:head>
